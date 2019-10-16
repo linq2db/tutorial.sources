fcc39683703837c7dfc5f400abb4bfc8982fa318
ili <chudinia@gmail.com>
2019-10-16 23:13:22 +0500

    Name and phone generator

--- linq2db.Tutorial.Sources/Tests/TestBase.cs
+++ linq2db.Tutorial.Sources/Tests/TestBase.cs
@@ -1,13 +1,25 @@
 ﻿using LinqToDB.Data;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace LinqToDB.Tutorial.Tests
 {
+	[TestFixture]
 	public class TestBase
 	{
+		private static readonly string[] MaleSurnames;
+		private static readonly string[] MaleNames;
+		private static readonly string[] FemaleSurnames;
+		private static readonly string[] FemaleNames;
+		private static readonly Random Random = new Random((int)(DateTime.Now.Ticks/1000));
+
 		/// <summary>
 		/// Осуществляет настройку конфигурации по умолчанию
 		/// </summary>
@@ -23,6 +35,58 @@ namespace LinqToDB.Tutorial.Tests
 
 			DataConnection.TurnTraceSwitchOn(TraceLevel.Info);
 			DataConnection.WriteTraceLine = (s1, s2) => Console.WriteLine($"{s1}: {s2}");
+
+			var assembly = typeof(TestBase).Assembly;
+			var names = assembly.GetManifestResourceNames();
+
+			MaleSurnames = ReadStrings(assembly, GetFileName(names, "MaleSurname.txt")).ToArray();
+			MaleNames = ReadStrings(assembly, GetFileName(names, "MaleName.txt")).ToArray();
+
+			FemaleSurnames = ReadStrings(assembly, GetFileName(names, "FemaleSurname.txt")).ToArray();
+			FemaleNames = ReadStrings(assembly, GetFileName(names, "FemaleName.txt")).ToArray();
+		}
+
+		private static string GetFileName(string[] names, string name)
+		{
+			var loc = CultureInfo.CurrentCulture.TwoLetterISOLanguageName.ToLower();
+			return names.FirstOrDefault(_ => _.EndsWith($"{loc}.{name}"))
+				?? names.First(_ => _.EndsWith($"en.{name}"));
+		}
+
+		private static IEnumerable<string> ReadStrings(Assembly assembly, string resourceName)
+		{
+			using (var stream = assembly.GetManifestResourceStream(resourceName))
+			using (var reader = new StreamReader(stream))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					yield return line;
+				}
+			}
+		}
+
+		protected static string GenerateName()
+		{
+			var m = Random.Next(1, 100);
+
+			var surnames = m <= 50 ? MaleSurnames : FemaleSurnames;
+			var names = m <= 50 ? MaleNames : FemaleNames;
+
+			var surname = surnames[Random.Next(0, surnames.Length - 1)];
+			var name = names[Random.Next(0, surnames.Length - 1)];
+
+			return $"{name} {surname}";
+		}
+
+		protected static string GeneratePhone()
+		{
+			var p1 = Random.Next(100, 999);
+			var p2 = Random.Next(0, 999); 
+			var p3 = Random.Next(0, 99);
+			var p4 = Random.Next(0, 99);
+
+			return $"{Properties.Resources.PhonePrefix} {p1} {p2} {p3} {p4}";
 		}
 	}
 }
---
+++ linq2db.Tutorial.Sources/Tests/ToolsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqToDB.Tutorial.Tests
+{
+	public class ToolsTests : TestBase
+	{
+		[Test]
+		public void GenerateTest()
+		{
+			var name = GenerateName();
+			var phone = GeneratePhone();
+
+			Console.WriteLine($"{name}: {phone}");
+
+			Assert.IsNotEmpty(name);
+			Assert.IsNotEmpty(phone);
+		}
+	}
+}
