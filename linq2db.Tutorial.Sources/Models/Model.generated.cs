3818bfa6ed819059e2e107633e5fba078dfa46ae
ili <chudinia@gmail.com>
2019-12-07 21:44:06 +0500

    Insert test

--- linq2db.Tutorial.Sources/Models/Model.generated.cs
+++ linq2db.Tutorial.Sources/Models/Model.generated.cs
@@ -8,6 +8,7 @@
 #pragma warning disable 1591
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using LinqToDB;
@@ -22,7 +23,8 @@ namespace LinqToDB.Tutorial.Models
 	/// </summary>
 	public partial class TutorialDataConnection : LinqToDB.Data.DataConnection
 	{
-		public ITable<Customer> Customers { get { return this.GetTable<Customer>(); } }
+		public ITable<BonusTransaction> BonusTransactions { get { return this.GetTable<BonusTransaction>(); } }
+		public ITable<Customer>         Customers         { get { return this.GetTable<Customer>(); } }
 
 		public TutorialDataConnection()
 		{
@@ -41,6 +43,24 @@ namespace LinqToDB.Tutorial.Models
 		partial void InitMappingSchema();
 	}
 
+	[Table("BonusTransaction")]
+	public partial class BonusTransaction
+	{
+		[Column, NotNull] public DateTime Time       { get; set; } // datetime
+		[Column, NotNull] public long     CustomerId { get; set; } // integer
+		[Column, NotNull] public decimal  Amount     { get; set; } // decimal
+
+		#region Associations
+
+		/// <summary>
+		/// FK_BonusTransaction_0_0
+		/// </summary>
+		[Association(ThisKey="CustomerId", OtherKey="Id", CanBeNull=false, Relationship=Relationship.ManyToOne, KeyName="FK_BonusTransaction_0_0", BackReferenceName="BonusTransactions")]
+		public Customer Customer { get; set; }
+
+		#endregion
+	}
+
 	[Table("Customer")]
 	public partial class Customer : IId
 	{
@@ -49,6 +69,17 @@ namespace LinqToDB.Tutorial.Models
 		[Column,                    NotNull    ] public string   Phone            { get; set; } // varchar(15)
 		[Column(SkipOnInsert=true), NotNull    ] public DateTime RegistrationTime { get; set; } // datetime
 		[Column,                       Nullable] public long?    Number           { get; set; } // integer
+		[Column,                       Nullable] public decimal? BonusAmount      { get; set; } // decimal
+
+		#region Associations
+
+		/// <summary>
+		/// FK_BonusTransaction_0_0_BackReference
+		/// </summary>
+		[Association(ThisKey="Id", OtherKey="CustomerId", CanBeNull=true, Relationship=Relationship.OneToMany, IsBackReference=true)]
+		public IEnumerable<BonusTransaction> BonusTransactions { get; set; }
+
+		#endregion
 	}
 
 	public static partial class TableExtensions
